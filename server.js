e196255ae2920ebe0a7aa36fa950825982402903
Yosef Hershberg <Berioo2000@gmail.com>
2023-06-19 12:59:38 +0300

    some stuff

--- server.js
+++ server.js
@@ -6,71 +6,96 @@ const qrcode = require('qrcode-terminal')
 const fs = require('fs')
 const app = require('./app.js')
 const router = express.Router()
+const cors = require('cors')
+
+app.use(cors());
 
 require('dotenv').config()
 
-const client = new Client({
-  authStrategy: new LocalAuth({ clientId: 'client-one' }),
-})
+app.get('/api/login', (req, res) => {
+  const { cleantIDMofo } = req.body
+  res.setHeader('Cache-Control', 'no-cache');
+  res.setHeader('Content-Type', 'text/event-stream');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Connection', 'keep-alive');
+  res.flushHeaders(); // flush the headers to establish SSE with client
+  console.log('streaming');
 
-client.initialize()
-
-client.on('authenticated', (session) => {
-  console.log('AUTHENTICATED', session)
-})
-
-client.on('auth_failure', (msg) => {
-  // Fired if session restore was unsuccessful
-  console.error('AUTHENTICATION FAILURE', msg)
-})
-
-client.on('ready', (res) => {
-  // res.write(`data: ready!\n\n`)
-  console.log('READY')
-})
-
-client.on('qr', (qr) => {
-  // NOTE: This event will not be fired if a session is specified.
-  console.log('QR RECEIVED', qr)
-  qrcode.generate(qr, { small: true }) // Add this line
-
-  app.get('/getqr', (req, res, next) => {
-    res.send({ qr })
+  // const client = new Client()
+  const client = new Client({
+    authStrategy: new LocalAuth({ clientId: 'client100' }),
   })
-})
 
-client.on('loading_screen', (percent, message) => {
-  app.get('/getload', (req, res, next) => {
-    res.setHeader('Cache-Control', 'no-cache')
-    res.setHeader('Content-Type', 'text/event-stream')
-    res.setHeader('Access-Control-Allow-Origin', '*')
-    res.setHeader('Connection', 'keep-alive')
-    res.flushHeaders() // flush the headers to establish SSE with client
-
-    const timeoutId = setTimeout(() => {
-      res.write(`data: scanned\n\n`)
-    }, 5000)
-    res.on('close', () => {
-      console.log('client dropped me')
-      clearTimeout(timeoutId)
-      res.end()
-    })
-    res.send(`Loading ${Math.round((100 * percent) / 2)}%`)
-  })
-  console.log('LOADING SCREEN', percent, message)
-})
-
-client.on('message', async (msg) => {
-  console.log('MESSAGE RECEIVED', msg)
-
-  if (msg.body === '!ping reply') {
-    // Send a new message as a reply to the current one
-    msg.reply('pong')
-  } else if (msg.body === 'ping') {
-    // Send a new message to the same chat
-    client.sendMessage(msg.from, 'pong')
+  client.initialize()
+  
+  const loadingMessage = {
+    message: 'loading_qr'
   }
-})
+
+  res.write(`data: ${JSON.stringify(loadingMessage)}\n\n`);
+
+  client.on('qr', (qr) => {
+    // NOTE: This event will not be fired if a session is specified.
+    console.log('QR RECEIVED', qr)
+    // qrcode.generate(qr, { small: true }) // Add this line
+
+    const payload = {
+      message: 'qr',
+      qrCode: qr
+    }
+    res.write(`data: ${JSON.stringify(payload)}\n\n`);
+  })
+
+  client.on('loading_screen', (percent, message) => {
+    console.log(message);
+    //--------------------------
+    const payload = {
+      message: 'scanned'
+    }
+    res.write(`data: ${JSON.stringify(payload)}\n\n`);
+    //---------------------------
+  })
+
+  client.on('ready', () => {
+    const payload = {
+      message: 'ready'
+    }
+    res.write(`data: ${JSON.stringify(payload)}\n\n`);
+  })
+
+
+  client.on('authenticated', (session) => {
+    const payload = {
+      message: 'authenticated'
+    }
+    res.write(`data: ${JSON.stringify(payload)}\n\n`);
+    console.log('AUTHENTICATED', session)
+  })
+
+  client.on('auth_failure', (msg) => {
+    // Fired if session restore was unsuccessful
+    console.error('AUTHENTICATION FAILURE', msg)
+  })
+
+  // If client closes connection, stop sending events
+  res.on('close', () => {
+    console.log('client dropped me');
+    // clearTimeout(timeoutId);
+    res.end();
+  });
+});
+
+// client.on('message', async (msg) => {
+//   console.log('MESSAGE RECEIVED', msg)
+
+//   if (msg.body === '!ping reply') {
+//     // Send a new message as a reply to the current one
+//     msg.reply('pong')
+//   } else if (msg.body === 'ping') {
+//     // Send a new message to the same chat
+//     client.sendMessage(msg.from, 'pong')
+//   }
+// })
 
 // Listening for the server
 const PORT = process.env.PORT || 3000
